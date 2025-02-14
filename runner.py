5814191a709dcbb5e97914d984cde87394eddfbe
rkemmey <rachel.kemmey@gmail.com>
2025-02-14 10:38:56 -0600

    2

--- runner.py
+++ runner.py
@@ -2,14 +2,6 @@ from classes.school import School
 
 school = School('Ridgemont High')
 
-
-#print(school.staff)
-#print(school.students)
-
-#runner.py 
-
-#mode = input("\nWhat would you like to do?\nOptions:\n1. List All Students\n2. View Individual Student <student_id>\n3. Add a Student\n4. Remove a Student <student_id>\n5. Quit\n")
-
 run = True
 while run:
     mode = input("\nWhat would you like to do?\nOptions:\n1. List All Students\n2. View Individual Student <student_id>\n3. Add a Student\n4. Remove a Student <student_id>\n5. Quit\n")
